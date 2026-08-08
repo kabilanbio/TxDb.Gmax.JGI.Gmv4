45cbc32d7f3cab93ae4b9eee6571e4ab1aa46f41
kabilanbio <kabilan151414@gmail.com>
2026-08-08 10:35:16 +0530

    last_commit

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,32 @@
-###
 ### Load any db objects whenever the package is loaded.
-###
-
 .onLoad <- function(libname, pkgname)
 {
   ns <- asNamespace(pkgname)
-  path <- system.file("extdata", package=pkgname, lib.loc=libname)
-  files <- dir(path)
-  for(i in seq_len(length(files))){
-    db <- AnnotationDbi::loadDb(system.file("extdata", files[[i]], package=pkgname,
-                                            lib.loc=libname),packageName=pkgname)
-    objname <- sub(".sqlite$","",files[[i]])
-    assign(objname, db, envir=ns)
-    namespaceExport(ns, objname)
+
+  # find installed extdata directory; system.file returns "" if not found
+  path <- system.file("extdata", package = pkgname, lib.loc = libname)
+  if (!nzchar(path) || !dir.exists(path)) {
+    return(invisible(NULL))
   }
+
+  # only consider .sqlite files
+  files <- dir(path, pattern = "\\.sqlite$", ignore.case = TRUE)
+  if (length(files) == 0L) return(invisible(NULL))
+
+  for (f in files) {
+    fpath <- file.path(path, f)
+    if (!file.exists(fpath)) next
+
+    # safe load: don't abort package load if a file is missing/corrupt
+    tryCatch({
+      db <- AnnotationDbi::loadDb(fpath, packageName = pkgname)
+      objname <- sub("\\.sqlite$", "", f, ignore.case = TRUE)
+      assign(objname, db, envir = ns)
+      namespaceExport(ns, objname)
+    }, error = function(e) {
+      packageStartupMessage(sprintf("Failed to load %s: %s", f, conditionMessage(e)))
+    })
+  }
+
+  invisible(NULL)
 }
